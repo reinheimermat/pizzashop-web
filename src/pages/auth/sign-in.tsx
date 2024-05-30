f4c7dc29d749b6d021d277cebb11bcbf45ffe24f
reinheimermat <mreinheimer0@gmail.com>
2024-05-30 09:49:51 -0300

    feat: add sonner toasts

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -23,6 +24,15 @@ export function SignIn() {
     console.log(data)
 
     await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    toast.success('Enviamos um link de autenticacão para o seu e-mail!', {
+      action: {
+        label: 'Reenviar',
+        onClick: () => {
+          handleSignIn(data)
+        },
+      },
+    })
   }
 
   return (
