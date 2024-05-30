43fc7b78346ab704ad05b0ac4f5a8cb5d1c33c16
reinheimermat <mreinheimer0@gmail.com>
2024-05-30 15:22:15 -0300

    feat: create orders list

---
+++ src/pages/app/orders/orders.tsx
@@ -0,0 +1,87 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+export function Orders() {
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+      </div>
+      <div className="space-y-2.5">
+        <form action="" className="flex items-center gap-2">
+          <span className="text-sm font-semibold">Filtros:</span>
+          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
+        </form>
+
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[140px]">Identificador</TableHead>
+                <TableHead className="w-[180px]">Realizado há</TableHead>
+                <TableHead className="w-[140px]">Status</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead className="w-[140px]">Total do pedido</TableHead>
+                <TableHead className="w-[164px]"></TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Button variant="outline" size="xs">
+                    <Search className="size-3" />
+                    <span className="sr-only">Detalhes do pedido</span>
+                  </Button>
+                </TableCell>
+                <TableCell className="font-mono text-xs font-medium">
+                  52ew2f151er5w1fw
+                </TableCell>
+                <TableCell className="text-muted-foreground">
+                  Há 15 minutos
+                </TableCell>
+                <TableCell>
+                  <div className="flex items-center gap-2">
+                    <span className="size-2 rounded-full bg-slate-400" />
+                    <span className="font-medium text-muted-foreground">
+                      Pendente
+                    </span>
+                  </div>
+                </TableCell>
+                <TableCell className="font-medium">
+                  Matheus De Souza Reinheimer
+                </TableCell>
+                <TableCell className="font-medium">R$ 149,90</TableCell>
+                <TableCell>
+                  <Button variant="outline" size="xs">
+                    <ArrowRight className="mr-2 size-3" />
+                    Aprovar
+                  </Button>
+                </TableCell>
+                <TableCell>
+                  <Button variant="ghost" size="xs">
+                    <X className="mr-2 size-3" />
+                    Cancelar
+                  </Button>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </>
+  )
+}
