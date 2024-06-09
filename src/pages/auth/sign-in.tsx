12c3005c5492da866889940ca32747f21b124837
reinheimermat <mreinheimer0@gmail.com>
2024-06-09 16:56:10 -0300

    feat: config client API

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,12 @@
 import { Label } from '@radix-ui/react-label'
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
@@ -21,19 +23,25 @@ export function SignIn() {
     formState: { isSubmitting },
   } = useForm<SignInForm>()
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
+    try {
+      await authenticate({ email: data.email })
 
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-
-    toast.success('Enviamos um link de autenticacão para o seu e-mail!', {
-      action: {
-        label: 'Reenviar',
-        onClick: () => {
-          handleSignIn(data)
+      toast.success('Enviamos um link de autenticacão para o seu e-mail!', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => {
+            handleSignIn(data)
+          },
         },
-      },
-    })
+      })
+    } catch {
+      toast.error('Ocorreu um erro ao enviar o link de autenticacão!')
+    }
   }
 
   return (
