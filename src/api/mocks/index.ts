5c2745e5da2d65d19f33372e66cd6e69fef8f592
Matheus <mreinheimer0@gmail.com>
2024-07-14 20:17:06 -0300

    feat: Update sign-in and register-restaurant mocks

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,10 +2,25 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
+import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amout-mock'
+import { getMonthCancelOrdersAmountMock } from './get-month-cancel-orders-amount-mock'
+import { getMonthOrdersAmountMock } from './get-month-orders-amout-mock'
+import { getMonthRevenueMock } from './get-month-revenue-mock'
+import { getPopularProductsMock } from './get-popular-products-mock'
 import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
 
-export const worker = setupWorker(signInMock, registerRestaurantMock)
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCancelOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
+)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
